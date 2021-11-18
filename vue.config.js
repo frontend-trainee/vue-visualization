7642c5f650c7298b58858a5d9f558a223ba50825
MleMoe <i@mle.moe>
2021-11-18 23:43:52 +0800

    :art: fix micro app config

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,24 @@
 const CompressionPlugin = require("compression-webpack-plugin");
 
 const path = require("path");
-const {
-  name
-} = require("./package");
+const { name } = require("./package");
 
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ?
-    process.env.VUE_APP_PUBLIC_PATH : "/",
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? process.env.VUE_APP_PUBLIC_PATH
+      : "/",
   outputDir: "dist", // 打包的目录
   lintOnSave: true, // 在保存时校验格式
   productionSourceMap: false, // 生产环境是否生成 SourceMap
   devServer: {
     open: true, // 启动服务后是否打开浏览器
     host: "0.0.0.0",
-    port: 8080, // 服务端口
+    port: 8082, // 服务端口
     https: false,
     hotOnly: false,
     proxy: null, // 设置代理
@@ -54,5 +54,5 @@ module.exports = {
       libraryTarget: "umd", // 把微应用打包成 umd 库格式
       jsonpFunction: `webpackJsonp_${name}`,
     },
-  }
+  },
 };
