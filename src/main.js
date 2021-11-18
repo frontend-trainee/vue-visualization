ffefe3553e04a981fea02639e381620029c66170
hardingcheng <hardingcheng@163.com>
2021-11-18 20:45:37 +0800

    按照微应用配置相关信息

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-Vue.config.productionTip = false;
-
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 
@@ -15,8 +13,42 @@ import "./static/font/font.css";
 //引入icon-font
 import "@/static/icon/iconfont.css";
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+Vue.config.productionTip = false;
+
+let instance = null;
+
+function render(props = {}) {
+  const {
+    container
+  } = props;
+
+  instance = new Vue({
+    router,
+    store,
+    render: (h) => h(App),
+  }).$mount(container ? container.querySelector("#app") : "#app");
+}
+
+// 独立运行时
+if (!(window).__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export async function bootstrap() {
+  console.log("[vue] vue app bootstraped");
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export async function mount(props) {
+  console.log("[vue] props from main framework", props);
+  render(props);
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export async function unmount() {
+  instance.$destroy();
+  instance.$el.innerHTML = "";
+  instance = null;
+  // router = null;
+}
--- src/router/index.js
+++ src/router/index.js
@@ -3,20 +3,20 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: "/",
+const prefix = (window).__POWERED_BY_QIANKUN__ ?
+  process.env.VUE_APP_MICRO_ROUTER :
+  "";
+const routes = [{
+    path: prefix + "/",
     name: "Home",
     component: Home,
     redirect: "/dashboard/overview",
   },
   {
-    path: "/dashboard",
+    path: prefix + "/dashboard",
     name: "Dashboard",
     component: () => import("../views/Dashboard"),
-    children: [
-      {
+    children: [{
         path: "overview",
         name: "Overview",
         component: () => import("../views/Dashboard/Overview"),
@@ -35,5 +35,19 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
+
+// 判断 qiankun 环境则进行路由拦截，判断跳转路由是否有 micro 应用开头前缀，没有则加上
+if ((window).__POWERED_BY_QIANKUN__) {
+  router.beforeEach((to, from, next) => {
+    if (!to.path.includes(prefix)) {
+      next({
+        path: prefix + to.path,
+      });
+    } else {
+      next();
+    }
+  });
+}
+
 
 export default router;
