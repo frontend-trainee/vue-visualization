50a449c90187405586076521b68ee30aa990d9ea
hardingcheng <hardingcheng@163.com>
2021-11-18 20:21:27 +0800

    微应用配置

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,17 @@
 const CompressionPlugin = require("compression-webpack-plugin");
 
 const path = require("path");
+const {
+  name
+} = require("./package");
 
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
 
 module.exports = {
-  publicPath: "/",
+  publicPath: process.env.NODE_ENV === "production" ?
+    process.env.VUE_APP_PUBLIC_PATH : "/",
   outputDir: "dist", // 打包的目录
   lintOnSave: true, // 在保存时校验格式
   productionSourceMap: false, // 生产环境是否生成 SourceMap
@@ -19,6 +23,9 @@ module.exports = {
     hotOnly: false,
     proxy: null, // 设置代理
     before: (app) => {},
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
   },
   chainWebpack: (config) => {
     /* 公共SCSS */
@@ -54,6 +61,11 @@ module.exports = {
             minRatio: 0.8, // 压缩比
           }),
         ],
+        output: {
+          library: `${name}-[name]`,
+          libraryTarget: "umd", // 把微应用打包成 umd 库格式
+          jsonpFunction: `webpackJsonp_${name}`,
+        },
       };
     }
   },
